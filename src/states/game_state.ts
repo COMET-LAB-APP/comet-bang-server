03640c2eb8a6a33fe49d8f1760d827f6b4b60f0c
Santa <arnaud.phommasone@gmail.com>
2024-08-18 22:47:39 +0700

    feat: add current timer ticks , turn,  position, action to game states

--- src/states/game_state.ts
+++ src/states/game_state.ts
@@ -5,5 +5,13 @@ interface GameState extends nkruntime.MatchState{
   isPrivate: boolean,
   gameState: GameStateEnum,
   emptyTicks: number,
-  joinsInProgress: number
+  joinsInProgress: number,
+  // timer Turn each turn will set this variable
+  deadlineRemainingTicks: number,
+  // current turn of player id 
+  currentTurnPlayerId ?: string,
+  // current turn of position 
+  currentPosition ?: number,
+  //current action state
+  currentActionState ?: ActionState,
 }

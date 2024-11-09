3a498da087e713d2e2478d45826f5f5c717d36fc
Santa <arnaud.phommasone@gmail.com>
2024-11-09 17:14:11 +0700

    fix: handle player action state

--- match_handler.ts
+++ match_handler.ts
@@ -182,54 +182,52 @@ const matchInit = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk
       // check timer turn count down 
       if (state.deadlineRemainingTicks <= 0) {
 
-        var currentTurnPlayerId = null;
-        var currentPosition = null;        
-        // inital start first turn
-        if (state.currentTurnPlayerId === null) {
-          // finding player first position with index 0
-          for (const userId in state.players) {
-            if (state.players.hasOwnProperty(userId) && state.players[userId].position == 0) {
-              currentTurnPlayerId = userId;
-              currentPosition = state.players[userId].position;
-              logger.info(`##T inital first turn ${userId}`)
+        // check next turn
+        switch (state.currentActionState) {
+          case ActionState.EffectPhase: 
+            toDrawPhase(state,dispatcher);
+            break;
+          case ActionState.DrawPhase: 
+            // TO main phase 
+            break;
+          case ActionState.MainPhase: 
+            toEndPhase(state,dispatcher,logger);
+            break;
+          case ActionState.AttackPhase: 
+             break;
+          case ActionState.SecondPhase: 
+            break;
+          case ActionState.EndPhase:
+            if(playeShouldDiscardCards(state)){
+              // Random discard card 
+              const player = state.players[state.currentTurnPlayerId];
+              const cardsLeft = removeRandomCards<Card>(player.cards, player.blood - player.cards.length);
+              state.players[state.currentTurnPlayerId].cards = cardsLeft;
+              // send update current cards 
+              state.currentActionState = ActionState.EndTurn;
+              const msg : ActionBase = {
+                currentPosition: state.currentPosition,
+                currentTurnPlayerId: state.currentTurnPlayerId,
+                turnTime: Math.floor(state.deadlineRemainingTicks / tickRate),
+                currentActionState: state.currentActionState,
+                metaData: {
+                  playerId: state.currentTurnPlayerId,
+                  cards: state.players[state.currentTurnPlayerId].cards
+                } 
+              }
+              dispatcher.broadcastMessage(OpCode.PLAYER_DRAW, JSON.stringify(msg)) ;
             }
-          }
-        } else {
-          // move turn to next player 
-          // check position with move around 
-          // TODO: should check currentPosition still alive or not
-          if(state.currentPosition < (state.requiredPlayerCount - 1) ){
-            currentPosition = state.currentPosition + 1;
-          } else {
-            currentPosition = 0;
-          }
+            // TODO: delay 
 
-          for (const userId in state.players) {
-            if (state.players.hasOwnProperty(userId) && state.players[userId].position == currentPosition) {
-              // assign currentTurnPlayerId  
-              currentTurnPlayerId = userId;
-              currentPosition = state.players[userId].position;
-            }
-          }
+            // TODO: Next Turn 
+            nextTurn(state,logger,dispatcher);
+            break;
+          default:
+            // default it does mean currentActionState isnot set init Action State
+            nextTurn(state,logger,dispatcher);
+            break;
         }
-
-        if (currentPosition != null && currentTurnPlayerId != null) {
-          state.currentPosition = currentPosition;
-          state.currentTurnPlayerId = currentTurnPlayerId;
-        }
-      
-        state.deadlineRemainingTicks = calculateDeadlineTicks(TimerTypeEnum.normal);
-        state.currentActionState = ActionState.DrawPhase
-        // object json message
-        let msg = {
-          currentPosition: state.currentPosition,
-          currentTurnPlayerId: state.currentTurnPlayerId,
-          turnTime: Math.floor(state.deadlineRemainingTicks / tickRate),
-          currentActionState : state.currentActionState
-        }
-        logger.info(`##T message current player turn :  ${JSON.stringify(msg)}}`)
-
-        dispatcher.broadcastMessage(OpCode.PLAYER_DRAW, JSON.stringify(msg))
+       
       } else {
         // count down timer turn
         state.deadlineRemainingTicks--;
@@ -256,7 +254,79 @@ const matchInit = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk
         case OpCode.PLAYER_DRAW : 
          //TODO : implement logic player draw
          break
-   
+        case OpCode.PLAYER_ACTION: 
+          // check card it there are target
+          //TODO: make sure can be PlayerActionCard
+
+          const playerAction = JSON.parse(`${message.data}`);
+          logger.info(`##T PLAYER_ACTION ${playerAction.actionState} ${playerAction.discardCardIds} is ${message.data}.`);
+          if (playerAction.cardId != null) {
+            if(playerAction.targetId != null){
+              // find card with CardId
+              const result = getCardById(playerAction.cardId);
+              if(result){
+                // check card types 
+                // global attack move to next player around (clockwise)
+                let body = {
+                  cards: result[0].damage,
+                  targetId : playerAction.targetId,
+                  CardTypes : result[0].type,
+                }
+                dispatcher.broadcastMessage(OpCode.PLAYER_TARGET, JSON.stringify(body))   
+              }
+            }
+            // else if player actions using card without target it does mean not cardType Attack 
+
+          } else if (playerAction.actionState == ActionState.EndPhase ) {
+
+            // player send cardIds for discard
+            const player = state.players[state.currentTurnPlayerId];
+            let allCards = player.cards as [Card]
+            const cardsLeft = allCards.filter((card) => playerAction.discardCardIds.indexOf(card.id) === -1)
+            state.players[state.currentTurnPlayerId].cards = cardsLeft;
+            logger.info(`##T PLAYER_ACTION discard and cards left ${cardsLeft.length} ${cardsLeft}.`);
+
+            state.currentActionState = ActionState.EndTurn;
+            const msg : ActionBase = {
+              currentPosition: state.currentPosition,
+              currentTurnPlayerId: state.currentTurnPlayerId,
+              turnTime: Math.floor(state.deadlineRemainingTicks / tickRate),
+              currentActionState: state.currentActionState,
+              metaData: {
+                playerId: state.currentTurnPlayerId,
+                cards: state.players[state.currentTurnPlayerId].cards
+              } 
+            }
+            dispatcher.broadcastMessage(OpCode.PLAYER_DRAW, JSON.stringify(msg));
+
+            // TODO: should delay before Next Turn 
+            nextTurn(state,logger,dispatcher)
+            
+          } else {
+            // skip phase
+            // emit player draw to next player 
+            // skip mainphase or not 
+               // server check how many card left > bloodsCount
+              if(playeShouldDiscardCards(state)){
+                // emit tell player to discard card 
+                state.currentActionState = ActionState.EndPhase;
+                state.deadlineRemainingTicks = calculateDeadlineTicks(TimerTypeEnum.normal);
+ 
+                let msg : ActionBase = {
+                  currentPosition: state.currentPosition,
+                  currentTurnPlayerId: state.currentTurnPlayerId,
+                  turnTime: Math.floor(state.deadlineRemainingTicks / tickRate),
+                  currentActionState: state.currentActionState,
+                }
+
+                dispatcher.broadcastMessage(OpCode.PLAYER_DRAW, JSON.stringify(msg))
+              }else {
+                //next turn
+                nextTurn(state,logger,dispatcher);
+              }
+          }
+          break
+        
       }
     }
     
@@ -331,6 +401,201 @@ const matchInit = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk
   // convert time milliseconds to seconds
   function msecToSec(n: number): number {
     return Math.floor(n / 1000);
-  }  function isBot(state: nkruntime.MatchState, playerId: string): boolean{
+  }
+
+  //TODO: algorithm 
+  function isGameEnded() : Boolean {
+    // check logic game end
+    return false;
+  }
+
+  //get Presence by playerId for send Broadcast message 
+  function getPrencesByPlayerIds(state: nkruntime.MatchState, playerIds: string[]): nkruntime.Presence[] {
+    var presences = [];
+    for (const userId in state.players) {
+      //check the player includes
+      if (state.players.hasOwnProperty(userId) && playerIds.indexOf(userId) !== -1) {
+        presences.push(state.players[userId].presence);
+      }
+    }
+    return presences;
+  }
+  // 
+  function isBot(state: nkruntime.MatchState, playerId: string): boolean{
     return state.players[playerId].isBot ?? false
   }
+
+  function playeShouldDiscardCards(state: nkruntime.MatchState): boolean{
+    let player = state.players[state.currentTurnPlayerId];
+    return player.blood < player.cards.length;
+  }
+
+  function nextTurn(state: nkruntime.MatchState,logger: nkruntime.Logger,dispatcher: nkruntime.MatchDispatcher) {
+    
+    var currentTurnPlayerId = null;
+    var currentPosition = null;        
+    // inital start first turn
+    if (state.currentTurnPlayerId === null) {
+      // finding player first position with index 0
+      for (const userId in state.players) {
+        if (state.players.hasOwnProperty(userId) && state.players[userId].position == 0) {
+          currentTurnPlayerId = userId;
+          currentPosition = state.players[userId].position;
+          logger.info(`##T inital first turn ${userId}`)
+        }
+      }
+    } else {
+      // move turn to next player 
+      // check position with move around 
+      // TODO: should check currentPosition still alive or not
+      if(state.currentPosition < (state.requiredPlayerCount - 1) ){
+        currentPosition = state.currentPosition + 1;
+      } else {
+        currentPosition = 0;
+      }
+
+      for (const userId in state.players) {
+        if (state.players.hasOwnProperty(userId) && state.players[userId].position == currentPosition) {
+          // assign currentTurnPlayerId  
+          currentTurnPlayerId = userId;
+          currentPosition = state.players[userId].position;
+        }
+      }
+    }
+
+    if (currentPosition != null && currentTurnPlayerId != null) {
+      state.currentPosition = currentPosition;
+      state.currentTurnPlayerId = currentTurnPlayerId;
+    }
+  
+    state.deadlineRemainingTicks = calculateDeadlineTicks(TimerTypeEnum.normal);
+    state.currentActionState = ActionState.EffectPhase
+    // object json message
+    let msg : ActionBase = {
+      currentPosition: state.currentPosition,
+      currentTurnPlayerId: state.currentTurnPlayerId,
+      turnTime:  Math.floor(state.deadlineRemainingTicks / tickRate),
+      currentActionState: state.currentActionState,
+    }
+    logger.info(`##T message current player turn :  ${JSON.stringify(msg)}}`)
+
+    dispatcher.broadcastMessage(OpCode.PLAYER_DRAW, JSON.stringify(msg))
+    
+    // set time out toDrawPhase
+    state.deadlineRemainingTicks = 1;
+  }
+
+
+  function toDrawPhase(state: nkruntime.MatchState, dispatcher: nkruntime.MatchDispatcher) {
+    let ishasEffect = false;
+    // check effect for tell current what to do
+    if (ishasEffect) { 
+      // check timeout then random action next phase
+      // action in when dinamite , snake ,.. 
+    } else {
+      // start draw card drawer + ability
+      state.currentActionState = ActionState.DrawPhase 
+      var cards = generateCards(2, mockCardsAttackAndDefence);
+
+      state.deadlineRemainingTicks = calculateDeadlineTicks(TimerTypeEnum.normal);
+      
+      let msg : ActionBase = {
+        currentPosition: state.currentPosition,
+        currentTurnPlayerId: state.currentTurnPlayerId,
+        turnTime: Math.floor(state.deadlineRemainingTicks / tickRate),
+        currentActionState: state.currentActionState,
+        metaData: {
+          cards: cards
+        } 
+      }
+      // filter player to specify send data 
+      // TODO: handle bot first
+      
+      if(!isBot(state,state.currentTurnPlayerId)){
+        let presences = getPrencesByPlayerIds(state,[state.currentTurnPlayerId]);
+        dispatcher.broadcastMessage(OpCode.PLAYER_DRAW, JSON.stringify(msg),presences) // self
+      }
+      
+      
+      // add delay and send action to every one 
+      // const delayInMilliseconds = 500;
+      // setTimeout(() => {
+        state.currentActionState = ActionState.MainPhase 
+        dispatcher.broadcastMessage(OpCode.PLAYER_DRAW, JSON.stringify({currentActionState: state.currentActionState}))
+      // },delayInMilliseconds)
+    }
+  }
+  
+  function toEndPhase(state: nkruntime.MatchState, dispatcher: nkruntime.MatchDispatcher,logger: nkruntime.Logger ) {
+    state.deadlineRemainingTicks = calculateDeadlineTicks(TimerTypeEnum.normal);
+    state.currentActionState = ActionState.EndPhase
+    // object json message
+    let msg : ActionBase = {
+      currentPosition: state.currentPosition,
+      currentTurnPlayerId: state.currentTurnPlayerId,
+      turnTime: Math.floor(state.deadlineRemainingTicks / tickRate),
+      currentActionState : state.currentActionState
+    }
+    logger.info(`##T toEndPhase :  ${JSON.stringify(msg)}}`)
+
+    dispatcher.broadcastMessage(OpCode.PLAYER_DRAW, JSON.stringify(msg))
+  }
+
+  function removeRandomCards<T>(array: T[], count: number): T[] {
+    if (count >= array.length) {
+      // If the count is greater than or equal to the array length, return an empty array.
+      return [];
+    }
+  
+    // Shuffle the array using Fisher-Yates algorithm.
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  
+    // Remove the first 'count' items from the shuffled array.
+    return array.slice(count);
+  }
+  
+  function getCardById(id: number): Card[] {
+    const filteredCards: Card[] = [
+      ...mockCards.filter(card => card.id === id),
+      ...mockCardsAttackAndDefence.filter(card => card.id === id)
+    ];
+    return filteredCards;
+  }
+
+
+function haveOnlyBotLeft(state: nkruntime.MatchState,logger: nkruntime.Logger,  ) : boolean  {
+  for (const userId in state.players) {
+    //check the player includes
+    logger.info("user leave check bot ",state.players[userId].isBot);
+    if (state.players.hasOwnProperty(userId) && !state.players[userId].isBot) {
+  
+      return false;
+    } 
+  }
+  return true;
+}
+/* 
+  TimeOut 
+  * Effect phase timeout
+    - random action before next phase 
+  * Draw phase timeout
+    - random action before next phase 
+  * Main phase 
+    - if skip Main phase with timeout
+    - then set state Endphase 
+
+  * Attack phase 
+    - random action before next phase 
+    - know target player A and player B , got hit and next phase to player A 
+
+  * Second phase 
+    - cannot use acttack 
+    - if timeout send setState endphase to server 
+  * Endphase 
+    - if player should discard equal true
+    - then random discard 
+    - next turn
+*/
